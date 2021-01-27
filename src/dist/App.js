7a6076b35effe5f4a32e08610a13ae84f5566663
moonorange <monoma632@gmail.com>
2021-01-27 23:18:59 +0900

    add: javascript to node.md

--- src/dist/App.js
+++ src/dist/App.js
@@ -7,10 +7,7 @@ function App() {
     return (react_1["default"].createElement("div", { className: "App" },
         react_1["default"].createElement("header", { className: "App-header" },
             react_1["default"].createElement("img", { src: logo_svg_1["default"], className: "App-logo", alt: "logo" }),
-            react_1["default"].createElement("p", null,
-                "Edit ",
-                react_1["default"].createElement("code", null, "src/App.tsx"),
-                " and save to reload."),
+            react_1["default"].createElement("p", null, "Hello World!"),
             react_1["default"].createElement("a", { className: "App-link", href: "https://reactjs.org", target: "_blank", rel: "noopener noreferrer" }, "Learn React"))));
 }
 exports["default"] = App;
